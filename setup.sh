f3c5598bbe9bed82abbf051d374a2e23102e49ca
summermick <john.he@horoppa.com>
2017-12-24 10:11:47 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,11 +9,6 @@ mkdir /opt/pycharm /opt/phpstorm /opt/idea-IU /opt/android-studio /opt/sublime_t
 bash ./provision.sh>install.log;
 bash ./create-certificate.sh default >>install.log;
 bash ./serve-laravel.sh default /home/chex/html 80 433 7.1 >>install.log;
-# setup npm
-mkdir /home/chex/.npm-global;
-npm config set prefix '/home/chex/.npm-global';
-printf "\nexport PATH=/home/chex/.npm-global/bin:$PATH;\n">>/home/chex/.profile;
-source /home/chex/.profile;
 # setup oh-my-zsh
 sed -i 's/robbyrussell/geoffgarside/' /home/chex/.zshrc;
 chsh -s /bin/zsh chex;
