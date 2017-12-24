c91716cd560174720520c31bf5785fa1e38dd9c9
summermick <john.he@horoppa.com>
2017-12-24 09:52:13 -0800

    Update setup.sh
    create install log

--- setup.sh
+++ setup.sh
@@ -6,10 +6,9 @@ touch /home/chex/.bash_aliases;
 # prep /opt
 mkdir /opt/pycharm /opt/phpstorm /opt/idea-IU /opt/android-studio /opt/sublime_text /opt/postman /opt/tor-browser /opt/datagrip /opt/matlab -p;
 # external scripts
-chmod +x create-certificate.sh serve-laravel.sh provision.sh;
-bash ./provision.sh;
-bash ./create-certificate.sh default;
-bash ./serve-laravel.sh default /home/chex/html 80 433 7.1;
+bash ./provision.sh>install.log;
+bash ./create-certificate.sh default >>install.log;
+bash ./serve-laravel.sh default /home/chex/html 80 433 7.1 >>install.log;
 # setup npm
 mkdir /home/chex/.npm-global;
 npm config set prefix '/home/chex/.npm-global';
